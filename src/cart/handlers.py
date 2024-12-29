c92bea98104fef59a38eb1e804b440fb5332416e
Lizaveta Yurashevich <lizaveta.yurashevich@gmail.com>
2024-12-29 14:38:40 +0300

    обновление и удаление товара из корзины, обработка ошибок

--- src/cart/handlers.py
+++ src/cart/handlers.py
@@ -1,10 +1,17 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from cart.queries import orm_get_cart, orm_add_product_to_cart
+from cart.queries import (
+    orm_get_cart,
+    orm_add_product_to_cart,
+    orm_delete_product_from_cart,
+    orm_update_product_quantity_in_cart,
+)
 from database import get_session
 from security.token import get_current_user
 from users.models import User
+
+from cart.queries import ProductNotFound, InsufficientStock
 
 router = APIRouter(prefix="/cart", tags=["cart"])
 
@@ -23,12 +30,54 @@ async def get_cart(
     return cart
 
 
-@router.post("/add")
+@router.post("/add-product")
 async def add_product_to_cart(
     product_id: int,
     quantity: int = 1,
     session: AsyncSession = Depends(get_session),
     user: User = Depends(get_current_user),
 ):
-    cart = await orm_add_product_to_cart(session, user.id, product_id, quantity)
+    try:
+        cart = await orm_add_product_to_cart(session, user.id, product_id, quantity)
+        return cart
+    except ProductNotFound:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="product not found"
+        )
+    except InsufficientStock as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=e.message)
+
+
+@router.delete("/delete-product")
+async def delete_product_from_cart(
+    product_id: int,
+    session: AsyncSession = Depends(get_session),
+    user: User = Depends(get_current_user),
+):
+    cart = await orm_delete_product_from_cart(session, user.id, product_id)
+
+    if not cart:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="cart not found"
+        )
+
+    return cart
+
+
+@router.post("/update-quantity")
+async def update_product_quantity_in_cart(
+    product_id: int,
+    quantity: int = 1,
+    session: AsyncSession = Depends(get_session),
+    user: User = Depends(get_current_user),
+):
+    cart = await orm_update_product_quantity_in_cart(
+        session, user.id, product_id, quantity
+    )
+
+    if not cart:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="cart not found"
+        )
+
     return cart
--- src/cart/queries.py
+++ src/cart/queries.py
@@ -2,32 +2,121 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from cart.models import Cart, CartItem
+from products.queries import orm_get_product_by_id
+
+from exceptions import ProductNotFound, InsufficientStock
 
 
 async def orm_get_cart(session: AsyncSession, user_id: int):
     query = select(Cart).where(Cart.user_id == user_id)
     result = await session.execute(query)
-    return result.scalar_one_or_none()
+    cart = result.scalar_one_or_none()
+
+    if not cart:
+        return None
+
+    # query = select(CartItem).where(CartItem.cart_id == cart.id)
+    # result = await session.execute(query)
+    # cart_items = result.scalars().all()
+
+    return cart
+
+    # products = []
+    # for item in cart_items:
+    #     product_data = await session.get(Product, item.product_id)
+    #     products.append(
+    #         {
+    #             'product_id': item.product_id,
+    #             'quantity': item.quantity,
+    #             'name': product_data.name,
+    #             'price': product_data.price
+    #         }
+    #     )
+    #
+    #     return {
+    #         'cart_id': cart.id,
+    #         'user_id': cart.user_id,
+    #         'items': products
+    #     }
 
 
 async def orm_add_product_to_cart(
     session: AsyncSession, user_id: int, product_id: int, quantity: int = 1
 ):
+
+    product = await orm_get_product_by_id(session, product_id)
+
+    if not product:
+        raise ProductNotFound
+
+    if product.stock < quantity:
+        raise InsufficientStock(
+            message=f"not enough stock for {product.name}. available: {product.stock}"
+        )
+
     cart = await orm_get_cart(session, user_id)
+
     if not cart:
         cart = Cart(user_id=user_id)
         session.add(cart)
         await session.commit()
         await session.refresh(cart)
+
     query = select(CartItem).where(
         CartItem.cart_id == cart.id, CartItem.product_id == product_id
     )
     result = await session.execute(query)
     cart_item = result.scalar_one_or_none()
+
     if cart_item:
         cart_item.quantity += quantity
     else:
         cart_item = CartItem(cart_id=cart.id, product_id=product_id, quantity=quantity)
         session.add(cart_item)
     await session.commit()
+
+    return cart
+
+
+async def orm_delete_product_from_cart(
+    session: AsyncSession, user_id: int, product_id: int
+):
+    cart = await orm_get_cart(session, user_id)
+
+    if not cart:
+        return None
+
+    query = select(CartItem).where(
+        CartItem.cart_id == cart.id, CartItem.product_id == product_id
+    )
+    result = await session.execute(query)
+    cart_item = result.scalar_one_or_none()
+
+    if cart_item:
+        await session.delete(cart_item)
+        await session.commit()
+
+    return cart
+
+
+async def orm_update_product_quantity_in_cart(
+    session: AsyncSession, user_id: int, product_id: int, quantity: int = 1
+):
+    cart = await orm_get_cart(session, user_id)
+
+    if not cart:
+        return None
+
+    query = select(CartItem).where(
+        CartItem.cart_id == cart.id, CartItem.product_id == product_id
+    )
+    result = await session.execute(query)
+    cart_item = result.scalar_one_or_none()
+
+    if not cart_item:
+        return None
+
+    cart_item.quantity = quantity
+    await session.commit()
+
     return cart
